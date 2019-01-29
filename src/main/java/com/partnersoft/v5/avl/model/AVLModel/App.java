a0f898ff7aee4fd0779823e5ebe2579305b2d26a
Lane Chasteen <lchasteen@gmail.com>
2019-01-29 18:59:44 -0500

    First Commit

---
+++ src/main/java/com/partnersoft/v5/avl/model/AVLModel/App.java
@@ -0,0 +1,13 @@
+package com.partnersoft.v5.avl.model.AVLModel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
